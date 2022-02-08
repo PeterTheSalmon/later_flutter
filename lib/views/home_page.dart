bba125099fae4d3253b8e4103ec1975c5c61c500
PeterTheSalmon <pdfsalmon@gmail.com>
2022-02-07 20:07:55 -0800

    updated home page drawer

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:later_flutter/services/authentication_service.dart';
 import 'package:later_flutter/views/folder_list.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,17 +16,51 @@ class HomePage extends StatelessWidget {
       drawer: Drawer(
         child: Column(
           children: [
-            Container(
+            SizedBox(
                 width: 310,
-                child: const DrawerHeader(
-                    decoration: BoxDecoration(color: Colors.orange),
-                    child: Text("Later",
-                        style: TextStyle(color: Colors.white, fontSize: 24)))),
+                height: 90,
+                child: DrawerHeader(
+                    decoration: const BoxDecoration(color: Colors.orange),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text("Later",
+                            style: TextStyle(fontSize: 24)),
+                        Text(FirebaseAuth.instance.currentUser!.email!)
+                      ],
+                    ))),
+            const ListTile(
+              title: Text("Account"),
+              leading: Icon(Icons.person),
+            ),
+            const ListTile(
+              title: Text("Settings"),
+              leading: Icon(Icons.settings),
+            ),
+            const ListTile(
+              title: Text("Manage Folders"),
+              leading: Icon(Icons.folder_open),
+            ),
+            const Divider(),
             const Expanded(child: FolderList())
           ],
         ),
       ),
-      body: const Text("Home"),
+      body: Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: Center(
+            child: Column(
+              children: [
+                const Text("Home", style: TextStyle(fontSize: 20)),
+                const Text("Current Unimplemented"),
+                TextButton(
+                    onPressed: () {
+                      context.read<AuthenticationService>().signOut();
+                    },
+                    child: const Text("Sign Out")),
+              ],
+            ),
+          )),
     );
   }
 }
