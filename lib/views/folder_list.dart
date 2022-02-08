fddd587aa097890d8d88efe47e44a79b6b6e7a06
PeterTheSalmon <pdfsalmon@gmail.com>
2022-02-07 19:34:16 -0800

    Implemented folder list

---
+++ lib/views/folder_list.dart
@@ -0,0 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class FolderList extends StatelessWidget {
+  const FolderList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection("folders").where("userId", isEqualTo: FirebaseAuth.instance.currentUser?.uid).snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) return const Text("Loading...");
+          return ListView(
+            children: snapshot.data!.docs.map((DocumentSnapshot document) {
+              return Column(
+                children: [
+                  ListTile(
+                    title: Text(document["name"]),
+                  ),
+                ],
+              );
+            }).toList(),
+          );
+        });
+  }
+
+}
---
+++ lib/views/sign_up_page.dart
@@ -0,0 +1,119 @@
+import 'package:later_flutter/services/authentication_service.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class SignUpPage extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController confirmEmailController = TextEditingController();
+
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
+
+  SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Create an Account"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                  suffixIcon: Icon(Icons.email),
+                  labelText: "Email",
+                  enabledBorder: UnderlineInputBorder()),
+              controller: emailController,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                  suffixIcon: Icon(Icons.email),
+                  labelText: "Confirm Email",
+                  enabledBorder: UnderlineInputBorder()),
+              controller: confirmEmailController,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                  suffixIcon: Icon(Icons.lock),
+                  labelText: "Password",
+                  enabledBorder: UnderlineInputBorder()),
+              controller: passwordController,
+              obscureText: true,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                  suffixIcon: Icon(Icons.lock),
+                  labelText: "Confirm Password",
+                  enabledBorder: UnderlineInputBorder()),
+              controller: confirmPasswordController,
+              obscureText: true,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton.icon(
+                onPressed: () {
+                  context.read<AuthenticationService>().signIn(
+                        emailController.text.trim(),
+                        passwordController.text.trim(),
+                      );
+                },
+                icon: const Icon(Icons.arrow_forward),
+                label: const Text("START"),
+              ),
+            ),
+            Consumer<AuthenticationService>(
+                builder: (context, authenticationService, child) => SizedBox(
+                    height: 50,
+                    child: Text(authenticationService.errorMessage ?? ""))),
+            const Text("By creating an acount you agree to the"),
+            const TermsPrivacyLinks(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TermsPrivacyLinks extends StatelessWidget {
+  const TermsPrivacyLinks({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        GestureDetector(
+          child: const Text("Privacy Policy",
+              style: TextStyle(color: Colors.blueAccent)),
+          onTap: () async {
+            const url =
+                "https://github.com/PeterTheSalmon/Later/blob/main/Later%20Privacy%20Policy%20-%20January%2031%202022.pdf";
+            if (await canLaunch(url)) {
+              launch(url, forceWebView: true, enableJavaScript: true);
+            }
+          },
+        ),
+        const Text(" and "),
+        GestureDetector(
+          child: const Text("Terms of Use",
+              style: TextStyle(color: Colors.blueAccent)),
+          onTap: () async {
+            const url =
+                "https://github.com/PeterTheSalmon/Later/blob/main/Later%20Terms%20of%20Use%20-%20January%2031%202022.pdf";
+            if (await canLaunch(url)) {
+              launch(url, forceWebView: true, enableJavaScript: true);
+            }
+          },
+        ),
+      ],
+    );
+  }
+}
