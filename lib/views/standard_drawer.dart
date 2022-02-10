1d93a6356483bf5a9665e3989f952a92783cffe6
PeterTheSalmon <pdfsalmon@gmail.com>
2022-02-09 20:50:39 -0800

    updated macos file versions and stuff

--- lib/views/standard_drawer.dart
+++ lib/views/standard_drawer.dart
@@ -11,63 +11,6 @@ class StandardDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return Column(
-    //   children: [
-    //     SizedBox(
-    //         width: 310,
-    //         height: 130,
-    //         child: DrawerHeader(
-    //             decoration: const BoxDecoration(color: Colors.orange),
-    //             child: Column(
-    //               crossAxisAlignment: CrossAxisAlignment.start,
-    //               children: [
-    //                 const Text("Later", style: TextStyle(fontSize: 24)),
-    //                 Text(FirebaseAuth.instance.currentUser!.email!)
-    //               ],
-    //             ))),
-    //     Expanded(
-    //       child: ListView(
-    //           padding: const EdgeInsets.only(top: 0),
-    //           children: [
-    //             ListTile(
-    //               title: const Text("Home"),
-    //               leading: const Icon(Icons.home),
-    //               onTap: () {
-    //                 Navigator.pop(context);
-    //                 Navigator.pushReplacement(
-    //                     context,
-    //                     MaterialPageRoute(
-    //                         builder: (context) => const HomePage()));
-    //               },
-    //             ),
-    //             const ListTile(
-    //               title: Text("Account"),
-    //               leading: Icon(Icons.person),
-    //             ),
-    //             const ListTile(
-    //               title: Text("Settings"),
-    //               leading: Icon(Icons.settings),
-    //             ),
-    //             SizedBox(
-    //               height: 60,
-    //               child: ListTile(
-    //                 title: const Text("Manage Folders"),
-    //                 leading: const Icon(Icons.folder_open),
-    //                 onTap: () {
-    //                   Navigator.pop(context);
-    //                   Navigator.pushReplacement(
-    //                       context,
-    //                       MaterialPageRoute(
-    //                           builder: (context) => const FolderManager()));
-    //                 },
-    //               ),
-    //             ),
-    //             const Divider(),
-    //             const Expanded(child: FolderList())
-    //           ]),
-    //     ),
-    //   ],
-    // );
     return ListView(padding: const EdgeInsets.only(top: 0), children: [
       SizedBox(
           width: 310,
