af3551ab97192ec1e45c3fc3a0a0fd93863f1dfa
PeterTheSalmon <pdfsalmon@gmail.com>
2022-02-12 15:17:50 -0800

    Updated app name on android, ios, and macos

--- lib/services/share_service.dart
+++ lib/services/share_service.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/services.dart';
 
 /// This service is responsible for talking with the OS to see if anything was
@@ -6,29 +8,35 @@ class ShareService {
   void Function(String)? onDataReceived;
 
   ShareService() {
-    // If sharing causes the app to be resumed, we'll check to see if we got any
-    // shared data
-    SystemChannels.lifecycle.setMessageHandler((msg) async {
-      if (msg?.contains("resumed") ?? false) {
-        getSharedData().then((String data) {
-          // Nothing was shared with us :(
-          if (data.isEmpty || data == "") {
-            return;
-          }
+    if (Platform.isAndroid || Platform.isIOS) {
+      // If sharing causes the app to be resumed, we'll check to see if we got any
+      // shared data
+      SystemChannels.lifecycle.setMessageHandler((msg) async {
+        if (msg?.contains("resumed") ?? false) {
+          getSharedData().then((String data) {
+            // Nothing was shared with us :(
+            if (data.isEmpty || data == "") {
+              return;
+            }
 
-          // We got something! Inform our listener.
-          onDataReceived?.call(data);
-        });
-      }
-      return;
-    });
+            // We got something! Inform our listener.
+            onDataReceived?.call(data);
+          });
+        }
+        return;
+      });
+    }
   }
 
   /// Invoke a method on our platform, telling it to give us any shared data
   /// it has
   Future<String> getSharedData() async {
-    return await const MethodChannel('com.petersalmon.later_flutter')
-            .invokeMethod("getSharedData") ??
-        "";
+    if (Platform.isAndroid || Platform.isIOS) {
+      return await const MethodChannel('com.petersalmon.later_flutter')
+              .invokeMethod("getSharedData") ??
+          "";
+    } else {
+      return "";
+    }
   }
 }
