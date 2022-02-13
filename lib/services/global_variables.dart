9abea68fdfe9fedd959e0c7d7394cd1c4d897843
PeterTheSalmon <pdfsalmon@gmail.com>
2022-02-12 17:27:39 -0800

    added tips to home page

--- lib/services/global_variables.dart
+++ lib/services/global_variables.dart
@@ -1,3 +1,11 @@
 class Globals {
   static String? sharedUrl;
+  static List<String> tipList = [
+    "Click the menu in the bottom right for quick actions!",
+    "Later is completely open source and free to use!",
+    "You can also share the app with your friends!",
+    "Check out the Mac app for a Mac version!",
+    "You can use Later on the web, on desktop, or on mobile!",
+    "Mmm tips"
+  ];
 }
