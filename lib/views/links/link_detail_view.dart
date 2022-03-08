fa1157b562a77b8330b1d0f62ad4221c5ee5c589
PeterTheSalmon <pdfsalmon@gmail.com>
2022-03-07 19:44:46 -0800

    updated edit link dialog. fixed scrolling bugs on link detail view

--- lib/views/links/link_detail_view.dart
+++ lib/views/links/link_detail_view.dart
@@ -5,6 +5,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:later_flutter/services/get_favicon.dart';
 import 'package:later_flutter/views/folders/folder_view.dart';
 import 'package:later_flutter/views/links/edit_link_dialog.dart';
@@ -29,6 +30,15 @@ class _LinkDetailViewState extends State<LinkDetailView> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.document['title']),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.copy),
+            onPressed: () {
+              Clipboard.setData(ClipboardData(text: widget.document['url']));
+            },
+            tooltip: "Copy URL",
+          )
+        ],
       ),
       bottomNavigationBar:
           BottomAppBar(child: _bottomNavigationBar(document: widget.document)),
@@ -40,7 +50,6 @@ class _LinkDetailViewState extends State<LinkDetailView> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                _faviconPreview(context),
                 Padding(
                   padding: const EdgeInsets.all(20.0),
                   child: Text(
@@ -51,6 +60,8 @@ class _LinkDetailViewState extends State<LinkDetailView> {
                   ),
                 ),
                 _websitePreview(context),
+                SizedBox(height: 20),
+                _notes(context)
               ],
             ),
           ),
@@ -59,70 +70,70 @@ class _LinkDetailViewState extends State<LinkDetailView> {
     );
   }
 
-  Widget _faviconPreview(BuildContext context) {
-    return (!kIsWeb)
-        ? Padding(
-            padding: const EdgeInsets.all(18.0),
-            child: CachedNetworkImage(
-              height: 80,
-              width: 80,
-              fit: BoxFit.fill,
-              imageUrl: getFavicon(widget.document["url"]),
-              placeholder: (context, url) => const CircularProgressIndicator(),
-              errorWidget: (context, url, error) => Container(),
-            ))
-        : Container();
+  Widget _notes(BuildContext context) {
+    final Map<String, dynamic> documentMap =
+        widget.document.data() as Map<String, dynamic>;
+    if (documentMap.containsKey('notes')) {
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(documentMap['notes']),
+      );
+    } else {
+      return TextButton(onPressed: () {}, child: Text("Add a note"));
+    }
   }
 
   Widget _websitePreview(BuildContext context) {
-    return (!kIsWeb)
-        ? SizedBox(
-            width: MediaQuery.of(context).size.width * 0.8,
-            height: MediaQuery.of(context).size.height * 0.3,
-            child: AnyLinkPreview(
-              link: widget.document['url'],
-              bodyMaxLines: 3,
-              showMultimedia: true,
-              backgroundColor:
+    if ((!kIsWeb)) {
+      return SizedBox(
+        width: MediaQuery.of(context).size.width * 0.8,
+        height: MediaQuery.of(context).size.height * 0.3,
+        child: AnyLinkPreview(
+          link: widget.document['url'],
+          bodyMaxLines: 3,
+          showMultimedia: true,
+          backgroundColor:
+              MediaQuery.of(context).platformBrightness == Brightness.dark
+                  ? const Color.fromARGB(255, 71, 71, 71)
+                  : const Color.fromARGB(255, 243, 243, 243),
+          titleStyle: TextStyle(
+              color:
                   MediaQuery.of(context).platformBrightness == Brightness.dark
-                      ? const Color.fromARGB(255, 71, 71, 71)
-                      : const Color.fromARGB(255, 243, 243, 243),
-              titleStyle: TextStyle(
-                  color: MediaQuery.of(context).platformBrightness ==
-                          Brightness.dark
                       ? Colors.white
                       : Colors.black),
-              placeholderWidget: Container(
-                  decoration: BoxDecoration(
-                    color: MediaQuery.of(context).platformBrightness ==
-                            Brightness.dark
+          placeholderWidget: Container(
+              decoration: BoxDecoration(
+                color:
+                    MediaQuery.of(context).platformBrightness == Brightness.dark
                         ? const Color.fromARGB(255, 71, 71, 71)
                         : const Color.fromARGB(255, 243, 243, 243),
-                    borderRadius: BorderRadius.circular(12),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey,
-                        blurRadius: 3,
-                      ),
-                    ],
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: [
+                  const BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 3,
                   ),
-                  child: const Center(
-                      child: Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: CircularProgressIndicator(),
-                  ))),
-              errorWidget: Container(
-                  decoration: BoxDecoration(
-                    color: MediaQuery.of(context).platformBrightness ==
-                            Brightness.dark
+                ],
+              ),
+              child: const Center(
+                  child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: CircularProgressIndicator(),
+              ))),
+          errorWidget: Container(
+              decoration: BoxDecoration(
+                color:
+                    MediaQuery.of(context).platformBrightness == Brightness.dark
                         ? const Color.fromARGB(255, 71, 71, 71)
                         : const Color.fromARGB(255, 243, 243, 243),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: const Center(child: Text("No Preview Available"))),
-            ),
-          )
-        : Container();
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: const Center(child: Text("No Preview Available"))),
+        ),
+      );
+    } else {
+      return Container();
+    }
   }
 
   Row _bottomNavigationBar({required DocumentSnapshot document}) {
@@ -160,76 +171,75 @@ class _LinkDetailViewState extends State<LinkDetailView> {
                 context: context,
                 builder: (context) => Dialog(
                         child: Container(
+                      height: 500,
                       constraints: const BoxConstraints(
                         maxWidth: 400,
                       ),
-                      child: Column(
+                      child: ListView(
+                        shrinkWrap: true,
                         children: [
                           const Text("Move to folder",
                               style: TextStyle(fontSize: 20)),
-                          Expanded(
-                            child: StreamBuilder<QuerySnapshot>(
-                                stream: FirebaseFirestore.instance
-                                    .collection("folders")
-                                    .where("userId",
-                                        isEqualTo: FirebaseAuth
-                                            .instance.currentUser!.uid)
-                                    .snapshots(),
-                                builder: (BuildContext context,
-                                    AsyncSnapshot<QuerySnapshot> snapshot) {
-                                  if (snapshot.hasError) {
-                                    return const Padding(
-                                      padding: EdgeInsets.all(8.0),
-                                      child: Text("Something went wrong :("),
-                                    );
-                                  }
-                                  if (snapshot.connectionState ==
-                                      ConnectionState.waiting) {
-                                    return const Padding(
-                                      padding: EdgeInsets.all(8.0),
-                                      child: Center(
-                                          child: CircularProgressIndicator()),
-                                    );
-                                  }
-                                  return Column(
-                                      children: snapshot.data!.docs
-                                          .map((DocumentSnapshot folder) {
-                                    return ListTile(
-                                      title: Text(folder["name"]),
-                                      leading: const Icon(Icons.folder),
-                                      onTap: () {
-                                        /// Moves the link to the tapped folder
-                                        FirebaseFirestore.instance
-                                            .collection("links")
-                                            .doc(document.id)
-                                            .update({
-                                          "parentFolderId": folder.id,
-                                        });
-                                        Navigator.pop(
-                                            context); // hide the dialog
-                                        Navigator.pop(
-                                            context); // hide the link detail view
-                                        Navigator.pushReplacement(
-                                            // open the folder it was moved to
-                                            context,
-                                            (FadeRoute(
-                                                page: FolderView(
-                                                    parentFolderId: folder.id,
-                                                    parentFolderName:
-                                                        folder["name"]))));
-                                        ScaffoldMessenger.of(context)
-                                            .showSnackBar(SnackBar(
-                                                content: Text(
-                                                    "Moved to ${folder["name"]}"),
-                                                action: SnackBarAction(
-                                                  onPressed: () {},
-                                                  label: "Close",
-                                                )));
-                                      },
-                                    );
-                                  }).toList());
-                                }),
-                          ),
+                          StreamBuilder<QuerySnapshot>(
+                              stream: FirebaseFirestore.instance
+                                  .collection("folders")
+                                  .where("userId",
+                                      isEqualTo: FirebaseAuth
+                                          .instance.currentUser!.uid)
+                                  .snapshots(),
+                              builder: (BuildContext context,
+                                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                                if (snapshot.hasError) {
+                                  return const Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child: Text("Something went wrong :("),
+                                  );
+                                }
+                                if (snapshot.connectionState ==
+                                    ConnectionState.waiting) {
+                                  return const Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child: Center(
+                                        child: CircularProgressIndicator()),
+                                  );
+                                }
+                                return Column(
+                                    children: snapshot.data!.docs
+                                        .map((DocumentSnapshot folder) {
+                                  return ListTile(
+                                    title: Text(folder["name"]),
+                                    leading: const Icon(Icons.folder),
+                                    onTap: () {
+                                      /// Moves the link to the tapped folder
+                                      FirebaseFirestore.instance
+                                          .collection("links")
+                                          .doc(document.id)
+                                          .update({
+                                        "parentFolderId": folder.id,
+                                      });
+                                      Navigator.pop(context); // hide the dialog
+                                      Navigator.pop(
+                                          context); // hide the link detail view
+                                      Navigator.pushReplacement(
+                                          // open the folder it was moved to
+                                          context,
+                                          (FadeRoute(
+                                              page: FolderView(
+                                                  parentFolderId: folder.id,
+                                                  parentFolderName:
+                                                      folder["name"]))));
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(SnackBar(
+                                              content: Text(
+                                                  "Moved to ${folder["name"]}"),
+                                              action: SnackBarAction(
+                                                onPressed: () {},
+                                                label: "Close",
+                                              )));
+                                    },
+                                  );
+                                }).toList());
+                              }),
                         ],
                       ),
                     )));
