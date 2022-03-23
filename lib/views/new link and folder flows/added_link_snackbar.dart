6a78f59213d8b739fdb0672ea9cb77b5ecce62c7
PeterTheSalmon <pdfsalmon@gmail.com>
2022-03-22 22:35:57 -0700

    update link snackbar

--- lib/views/new link and folder flows/added_link_snackbar.dart
+++ lib/views/new link and folder flows/added_link_snackbar.dart
@@ -3,19 +3,28 @@ import 'package:flutter/material.dart';
 import 'package:later_flutter/models/folder.dart';
 import 'package:later_flutter/views/folders/folder_view.dart';
 import 'package:later_flutter/views/styles/fade_route.dart';
+import 'package:later_flutter/views/styles/fade_through_route.dart';
 
+/// This function is an absolute disaster.
+/// The `BuildContext` is stale. It is passed in from a dialog,
 void showAddedLinkSnackbar(BuildContext context, String parentFolderId) async {
   final nav = Navigator.maybeOf(context);
+  final sm = ScaffoldMessenger.maybeOf(context);
+  final mq = MediaQuery.maybeOf(context);
+
+  /// if we messed up any of the maybeOf()'s, we can't do anything
+  if (nav == null || sm == null || mq == null) {
+    return;
+  }
+
+  final displayMobileLayout = mq.size.width < 550;
 
   final parentFolder = Folder.fromSnapshot(await FirebaseFirestore.instance
       .collection('folders')
       .doc(parentFolderId)
       .get());
 
-  if (nav == null) {
-    return;
-  }
-  ScaffoldMessenger.of(context).showSnackBar(
+  sm.showSnackBar(
     SnackBar(
       content: const Text("Added link!"),
       action: SnackBarAction(
@@ -24,12 +33,17 @@ void showAddedLinkSnackbar(BuildContext context, String parentFolderId) async {
             if (!nav.mounted) {
               return;
             }
-            nav.push(FadeRoute(
-              page: FolderView(
-                parentFolderId: parentFolderId,
-                parentFolderName: parentFolder.name,
-              ),
-            ));
+            nav.push(
+              displayMobileLayout
+                  ? fadeThrough((context, animation, secondaryAnimation) =>
+                      FolderView(
+                          parentFolderId: parentFolderId,
+                          parentFolderName: parentFolder.name))
+                  : FadeRoute(
+                      page: FolderView(
+                          parentFolderId: parentFolderId,
+                          parentFolderName: parentFolder.name)),
+            );
           }),
     ),
   );
